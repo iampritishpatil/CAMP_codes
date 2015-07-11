88d0ae78b473e90304de464b649c0d5196351757
Pritish Patil <iampritishpatil@gmail.com>
2015-07-11 11:53:42 +0530

    fixed bug in inhibition parameter

--- brain_model.py
+++ brain_model.py
@@ -4,16 +4,16 @@ import numpy as np
 import pylab as plt
 
 N=1000
-p=0.1
+p=0.03
 
 taum = 20 * brian.ms          # membrane time constant
 taue = 5 * brian.ms          # excitatory synaptic time constant
 taui = 10 * brian.ms          # inhibitory synaptic time constant
-Vt = -50 * brian.mV          # spike threshold
-Vr = -60 * brian.mV          # reset value
-El = -49 * brian.mV          # resting potential
-we = 400.0/N * brian.mV # excitatory synaptic weight
-wi = 400.0/N * brian.mV # inhibitory synaptic weight
+Vt = -40 * brian.mV          # spike threshold
+Vr = -65 * brian.mV          # reset value
+El = -65 * brian.mV          # resting potential
+we = 40.0/N /p * brian.mV # excitatory synaptic weight
+wi = 40.0/N /p * brian.mV # inhibitory synaptic weight
 
 
 mat=create_matrix(p,N)
@@ -23,26 +23,35 @@ map_inh=lambda i,j:wi*(mat[i,j]) if mat[i,j]<0 else 0*brian.mV
 
 eqs = brian.Equations('''
 
-        dV/dt  = (ge-gi-(V-El))/taum : volt
+        dV/dt  = (ge+gi-(V-El)+I)/taum  : volt
         dge/dt = -ge/taue            : volt
         dgi/dt = -gi/taui            : volt
+        I : volt
         ''')
 neurons = brian.NeuronGroup(N, model=eqs, threshold=Vt, reset=Vr)
 
-exc=brian.Connection(neurons, neurons, 'ge', sparseness=1, weight=map_exc, threshold=Vt, reset=Vr,refractory=2*brian.ms,delay=2*brian.ms)
+neurons.I=np.ones(N)*40*brian.mV
 
-inh=brian.Connection(neurons, neurons, 'gi', sparseness=1, weight=map_inh, threshold=Vt, reset=Vr,refractory=2*brian.ms,delay=2*brian.ms)
+exc=brian.Connection(neurons, neurons, 'ge', sparseness=1, weight=map_exc, threshold=Vt, reset=Vr,refractory=2*brian.ms,delay=1*brian.ms)
 
-
+inh=brian.Connection(neurons, neurons, 'gi', sparseness=1, weight=map_inh, threshold=Vt, reset=Vr,refractory=2*brian.ms,delay=1*brian.ms)
 
 
 spikes=brian.SpikeMonitor(neurons)
 
-neurons.V = Vr + np.random.rand(N) * (Vt - Vr)*1.1
+neurons.V = Vr + np.random.rand(N) * (Vt - Vr)*0.9
 
-brian.run(3*brian.second)
+print neurons.I
+
+brian.run(0.11*brian.second)
 #plt.subplot(211)
+neurons.I=0*np.ones(N)*brian.mV
+
+brian.run(0.5*brian.second)
+
 brian.raster_plot(spikes)
 plt.show()
 
+a=spikes.getspiketimes()
+b=a.values()
 
